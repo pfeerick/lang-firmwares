fbce249afc6fa3c4181a7045814f57511e725663
Peter Feerick <peter.feerick@gmail.com>
2023-02-19 12:32:47 +1000

    fix: Bluetooth build needs more handholding

--- build-firmwares-bluetooth.sh
+++ build-firmwares-bluetooth.sh
@@ -9,10 +9,12 @@ SRC_DIR="${SRC_DIR:=$(realpath "$(pwd)"/edgetx)}"
 # TARGET_NAMES_COLORLCD=("nv14" el18 "t16" "t18" "tx16s"
 # "x10" "x10-access" "x12s")
 
-# TARGET_NAMES_BW=("t8" "tlite" "tpro" "tx12" "tx12mk2"
-# "x7" "x7-access" "x9d" "x9dp" "x9dp2019"
-# "x9e" "x9e-hall" "x9lite" "x9lites" "xlite"
-# "xlites" "zorro" "commando8" "lr3pro" "t12")
+TARGET_NAMES_BW=("t8" "tlite" "tpro" "tx12" "tx12mk2"
+"x7" "x7-access" "x9d" "x9dp" "x9dp2019"
+"x9e" "x9e-hall" "x9lite" "x9lites" "xlite"
+"xlites" "zorro" "commando8" "lr3pro" "t12")
+
+TARGET_BLUETOOTH_SKIP=("t8" "tlite" "tpro" "commando8" "lr3pro" "t12")
 
 # Voice menu only: SK HU
 #LANGUAGES_ALL=(CN CZ DA DE EN ES FI FR IT JP PT SE TW PL NL)
@@ -20,7 +22,7 @@ LANGUAGES_COLORLCD=(CN CZ DA DE EN ES FI FR IT JP PT SE TW PL NL)
 
 # CZ overflowed on x9d family build
 # CN, TW and JP don't display properly on B&W when last checked
-# LANGUAGES_BW=(DA DE EN ES FI FR IT PT SE PL NL)
+LANGUAGES_BW=(CZ DA DE EN ES FI FR IT PT SE PL NL)
 
 # workaround for GH repo owner
 git config --global --add safe.directory "$(pwd)"
@@ -38,19 +40,23 @@ fi
 target_names=$(echo "$FLAVOR" | tr '[:upper:]' '[:lower:]' | tr ';' '\n')
 
 for target in $target_names; do
+    if [[ ${TARGET_BLUETOOTH_SKIP[*]} =~ ${target} ]]; then
+        break
+    fi
+
     fw_name="${target}-${GIT_SHA_SHORT}.bin"
 
-    # if [[ ${TARGET_NAMES_BW[*]} =~ ${target} ]]; then
-    #     for lang in "${LANGUAGES_BW[@]}"; do
-    #         SRCDIR=${SRC_DIR} FLAVOR=${target} EXTRA_OPTIONS="-DTRANSLATIONS=${lang} -DBLUETOOTH=Y " "${SRC_DIR}/tools/build-gh.sh"
-    #         mv "${fw_name}" "${target}-${lang}-heli-${GIT_SHA_SHORT}.bin"
-    #     done
-    # else # Color LCD
+    if [[ ${TARGET_NAMES_BW[*]} =~ ${target} ]]; then
+        for lang in "${LANGUAGES_BW[@]}"; do
+            SRCDIR=${SRC_DIR} FLAVOR=${target} EXTRA_OPTIONS="-DTRANSLATIONS=${lang} -DBLUETOOTH=Y " "${SRC_DIR}/tools/build-gh.sh"
+            mv "${fw_name}" "${target}-${lang}-bluetooth-${GIT_SHA_SHORT}.bin"
+        done
+    else # Color LCD
         for lang in "${LANGUAGES_COLORLCD[@]}"; do
             SRCDIR=${SRC_DIR} FLAVOR=${target} EXTRA_OPTIONS="-DTRANSLATIONS=${lang} -DBLUETOOTH=Y " "${SRC_DIR}/tools/build-gh.sh"
             mv "${fw_name}" "${target}-${lang}-bluetooth-${GIT_SHA_SHORT}.bin"
         done
-    # fi
+    fi
 
     # Zip all the languages for this target, remove the rest
     sha256sum "${target}"-[A-Z][A-Z]-*.bin >> "${target}".sha256 || continue
