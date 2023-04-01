38ac2ea74e4a636022e15abb732011ab38f268dc
Peter Feerick <peter.feerick@gmail.com>
2023-04-01 19:47:07 +1000

    chore: Conditionally omit x9dp2019 DE

--- build-firmwares.sh
+++ build-firmwares.sh
@@ -9,8 +9,8 @@ SRC_DIR="${SRC_DIR:=$(realpath "$(pwd)"/edgetx)}"
 # TARGET_NAMES_COLORLCD=("nv14" el18 "t16" "t18" "tx16s"
 # "x10" "x10-access" "x12s")
 
-TARGET_NAMES_BW=("t8" "tlite" "tpro" "tx12" "tx12mk2"
-"x7" "x7-access" "x9d" "x9dp" "x9dp2019"
+TARGET_NAMES_BW=("x9dp2019" "t8" "tlite" "tpro" "tx12"
+"tx12mk2" "x7" "x7-access" "x9d" "x9dp"
 "x9e" "x9e-hall" "x9lite" "x9lites" "xlite"
 "xlites" "zorro" "commando8" "lr3pro" "t12")
 
@@ -18,9 +18,8 @@ TARGET_NAMES_BW=("t8" "tlite" "tpro" "tx12" "tx12mk2"
 #LANGUAGES_ALL=(CN CZ DA DE EN ES FI FR IT JP PT SE TW PL NL)
 LANGUAGES_COLORLCD=(CN CZ DA DE EN ES FI FR IT JP PT SE TW PL NL)
 
-# CZ overflowed on x9d family build
 # CN, TW and JP don't display properly on B&W when last checked
-LANGUAGES_BW=(DA DE EN ES FI FR IT PT SE PL NL)
+LANGUAGES_BW=(CZ DA DE EN ES FI FR IT PT SE PL NL)
 
 # workaround for GH repo owner
 git config --global --add safe.directory "$(pwd)"
@@ -44,6 +43,8 @@ for target in $target_names; do
 
     if [[ ${TARGET_NAMES_BW[*]} =~ $re ]]; then
         for lang in "${LANGUAGES_BW[@]}"; do
+            if [[ "${target}" =~ ^x9d.* && "${lang}" == "CZ" ]]; then continue; fi # x9d family CZ overflow
+            if [[ "${target}" == "x9dp2019" && "${lang}" == "DE" ]]; then continue; fi # x9d+2019 DE overflow
             SRCDIR=${SRC_DIR} FLAVOR=${target} EXTRA_OPTIONS="-DTRANSLATIONS=${lang} " "${SRC_DIR}/tools/build-gh.sh"
             mv "${fw_name}" "${target}-${lang}-${GIT_SHA_SHORT}.bin"
         done
