58424ce06b9a2f4ed0bb0ee8444d2e0b959dc4b7
Peter Feerick <peter.feerick@gmail.com>
2023-04-03 12:29:13 +1000

    fix: 'PL' overflowing on x9d+2019

--- build-firmwares.sh
+++ build-firmwares.sh
@@ -44,6 +44,7 @@ for target in $target_names; do
     if [[ ${TARGET_NAMES_BW[*]} =~ $re ]]; then
         for lang in "${LANGUAGES_BW[@]}"; do
             if [[ "${target}" =~ ^x9d.* && "${lang}" == "CZ" ]]; then continue; fi # x9d family CZ overflow
+            if [[ "${target}" == "x9dp2019" && "${lang}" == "PL" ]]; then continue; fi # x9d+2019 PL overflow
             if [[ "${target}" == "x9dp2019" && "${lang}" == "DE" ]]; then continue; fi # x9d+2019 DE overflow
             SRCDIR=${SRC_DIR} FLAVOR=${target} EXTRA_OPTIONS="-DTRANSLATIONS=${lang} " "${SRC_DIR}/tools/build-gh.sh"
             mv "${fw_name}" "${target}-${lang}-${GIT_SHA_SHORT}.bin"
