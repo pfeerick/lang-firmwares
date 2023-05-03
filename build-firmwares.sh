aaca20c0894b911f65b3b55dcc8a5be1ce82b3bb
Peter Feerick <peter.feerick@gmail.com>
2023-05-03 19:00:39 +1000

    chore: DE overlow

--- build-firmwares.sh
+++ build-firmwares.sh
@@ -45,7 +45,7 @@ for target in $target_names; do
         for lang in "${LANGUAGES_BW[@]}"; do
             if [[ "${target}" =~ ^x9d.* && "${lang}" == "CZ" ]]; then continue; fi # x9d family CZ overflow
             # if [[ "${target}" == "x9dp2019" && "${lang}" == "DA" ]]; then continue; fi # x9d+2019 DA overflow
-            # if [[ "${target}" == "x9dp2019" && "${lang}" == "DE" ]]; then continue; fi # x9d+2019 DE overflow
+            if [[ "${target}" == "x9dp2019" && "${lang}" == "DE" ]]; then continue; fi # x9d+2019 DE overflow
             # if [[ "${target}" == "x9dp2019" && "${lang}" == "ES" ]]; then continue; fi # x9d+2019 ES overflow
             # if [[ "${target}" == "x9dp2019" && "${lang}" == "FI" ]]; then continue; fi # x9d+2019 FI overflow
             # if [[ "${target}" == "x9dp2019" && "${lang}" == "FR" ]]; then continue; fi # x9d+2019 FR overflow
