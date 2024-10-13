9309ea29e7856c3b379d659f8401abe6ec1c6f64
Peter Feerick <peter.feerick@gmail.com>
2024-10-13 11:56:58 +1000

    fix: maybe actually try to build new targets

--- build-firmwares-bluetooth-heli.sh
+++ build-firmwares-bluetooth-heli.sh
@@ -13,7 +13,8 @@ TARGET_NAMES_BW=("x9dp2019" "t8" "tlite" "tpro" "tx12"
 "tx12mk2" "x7" "x7-access" "x9d" "x9dp"
 "x9e" "x9e-hall" "x9lite" "x9lites" "xlite"
 "xlites" "zorro" "commando8" "lr3pro" "t12" 
-"pocket" "mt12" "t20" "t20v2" "t14" tprov2)
+"pocket" "mt12" "t20" "t20v2" "t14" "tprov2" 
+"t12max" "tpros" "bumblebee")
 
 TARGET_BLUETOOTH_ENABLED=("x7" "x7-access" "xlite" "x9lites" "x9dp2019"
 "t16" "t18" "tx16s" "x10" "x10-access" "x12s" "boxer" "tx12" "tx12mk2" "zorro")
